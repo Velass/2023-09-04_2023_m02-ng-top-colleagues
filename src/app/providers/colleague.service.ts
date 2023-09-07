35a6cd1fffd06e1249863c9ed3d0fd88f9bc9850
florian <furnari.florian@hotmail.fr>
2023-09-07 17:18:03 +0200

    vote coter collégue ok

--- src/app/providers/colleague.service.ts
+++ src/app/providers/colleague.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Colleague } from '../models/colleague';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ColleagueService {
+   httpOptions = {
+    headers: new HttpHeaders({
+    "Content-Type": "application/json"
+    })
+    };
   colleagues!: Colleague[] 
   private apiUrl = 'https://app-6f6e9c23-7f63-4d86-975b-a0b1a1440f94.cleverapps.io/api/v2/colleagues';
+  private apiUrl2 = "https://app-6f6e9c23-7f63-4d86-975b-a0b1a1440f94.cleverapps.io/api/v2/votes";
+
 
   constructor(private http: HttpClient) { 
 
   }
 
+  
+
+  vote(pseudo: string, like_hate: string): Observable<any> {
+    const voteData = { pseudo, like_hate };
+    return this.http.post(`${this.apiUrl2}`, voteData, this.httpOptions);
+
+  }
 
   getColleagues(): Observable<Colleague[]> {
     return this.http.get<Colleague[]>(this.apiUrl);
--- src/app/shared/components/colleague/colleague.component.ts
+++ src/app/shared/components/colleague/colleague.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Colleague } from 'src/app/models/colleague';
 import { LikeHate } from 'src/app/models/like-hate';
+import { ColleagueService } from 'src/app/providers/colleague.service';
 import { VoteService } from 'src/app/providers/vote.service';
 
 @Component({
@@ -16,16 +17,24 @@ export class ColleagueComponent {
   disableHateButton = false;
 
 
-  constructor(private voteService : VoteService) {
+  constructor(private voteService : VoteService,private colleagueService : ColleagueService) {
 
   }
+  
   handleAvis(avis: LikeHate) {
     if (this.colleague) {
       if (avis === LikeHate.LIKE) {
         this.voteService.actionNext({colleague : this.colleague, vote: LikeHate.LIKE})
+        this.colleagueService.vote(this.colleague.pseudo, "LIKE").subscribe(res => {
+          this.colleague.score = res.score
+        });
         this.colleague.score++;
       } if (avis === LikeHate.HATE) {
         this.voteService.actionNext({colleague : this.colleague, vote: LikeHate.HATE})
+        this.colleagueService.vote(this.colleague.pseudo, "HATE").subscribe(res => {
+          console.log(res)
+          this.colleague.score = res.score
+        });
         this.colleague.score--;
       }
       if (this.colleague.score >= 1000) {
