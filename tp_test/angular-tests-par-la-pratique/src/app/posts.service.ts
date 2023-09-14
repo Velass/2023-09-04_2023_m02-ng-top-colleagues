533e540ee8d72913dcd9e1cd0e843346e3cae9d8
florian <furnari.florian@hotmail.fr>
2023-09-14 16:39:41 +0200

    testservice ok

--- tp_test/angular-tests-par-la-pratique/src/app/posts.service.ts
+++ tp_test/angular-tests-par-la-pratique/src/app/posts.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
 export class PostsService {
-
-  constructor() { }
+// TODO injection du service HttpClient
+constructor(private http: HttpClient) { }
+// TODO ajouter la méthode "recupererTousLesPosts"
+recupererTousLesPosts(): Observable<any> {
+return this.http.get("https://jsonplaceholder.typicode.com/posts");
+}
 }
